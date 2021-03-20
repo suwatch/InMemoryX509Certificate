a26af2c14cd6fbed57be53a63e0b3052944eed2c
Suwath Ch <suwatch@microsoft.com>
2021-03-19 20:29:20 -0700

    Initial Commit

---
+++ InMemoryX509Certificate.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace InMemoryX509Certificate
+{
+    public class InMemoryX509Certificate : X509Certificate
+    {
+        const uint EphemeralKeySetFlag = 0x8200;
+        static readonly Type X509UtilsType = typeof(X509Certificate).Assembly.GetType("System.Security.Cryptography.X509Certificates.X509Utils", throwOnError: true);
+        static readonly MethodInfo LoadCertFromFile = X509UtilsType.GetMethod("_LoadCertFromFile", BindingFlags.Static | BindingFlags.NonPublic);
+        static readonly MethodInfo LoadCertFromBlob = X509UtilsType.GetMethod("_LoadCertFromBlob", BindingFlags.Static | BindingFlags.NonPublic);
+        static readonly FieldInfo SafeCertContextField = typeof(X509Certificate).GetField("m_safeCertContext", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        readonly WeakReference m_safeCertContext;
+
+        public InMemoryX509Certificate(byte[] rawData, string password)
+        {
+            m_safeCertContext = new WeakReference(SafeCertContextField.GetValue(this));
+            LoadCertificateFromBlob(rawData, password);
+        }
+
+        public InMemoryX509Certificate(string fileName, string password)
+        {
+            m_safeCertContext = new WeakReference(SafeCertContextField.GetValue(this));
+            LoadCertificateFromFile(fileName, password);
+        }
+
+        private void LoadCertificateFromBlob(byte[] rawData, string password)
+        {
+            IntPtr intPtr = IntPtr.Zero;
+            RuntimeHelpers.PrepareConstrainedRegions();
+            try
+            {
+                intPtr = Marshal.StringToHGlobalUni(password);
+                LoadCertFromBlob.Invoke(null, new object[] { rawData, intPtr, EphemeralKeySetFlag, false, m_safeCertContext.Target });
+            }
+            finally
+            {
+                if (intPtr != IntPtr.Zero)
+                {
+                    Marshal.ZeroFreeGlobalAllocUnicode(intPtr);
+                }
+            }
+        }
+
+        private void LoadCertificateFromFile(string fileName, string password)
+        {
+            IntPtr intPtr = IntPtr.Zero;
+            RuntimeHelpers.PrepareConstrainedRegions();
+            try
+            {
+                intPtr = Marshal.StringToHGlobalUni(password);
+                LoadCertFromFile.Invoke(null, new object[] { fileName, intPtr, EphemeralKeySetFlag, false, m_safeCertContext.Target });
+            }
+            finally
+            {
+                if (intPtr != IntPtr.Zero)
+                {
+                    Marshal.ZeroFreeGlobalAllocUnicode(intPtr);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace InMemoryX509Certificate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var pfx = args[0];
+                var pwd = args[1];
+                if (File.Exists(pwd))
+                {
+                    pwd = File.ReadAllText(pwd.Trim());
+                }
+
+                for (int i = 0; i < 3; ++i)
+                {
+                    using (var inmem = new InMemoryX509Certificate(pfx, pwd))
+                    using (var cert = new X509Certificate2(inmem.Handle))
+                    {
+                        Console.WriteLine($"{i + 1}. Thumbprint: {cert.Thumbprint}, HasPrivateKey: {cert.HasPrivateKey}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+
+            }
+        }
+    }
+}
