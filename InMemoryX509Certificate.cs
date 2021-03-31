89e82646c8156bd8152873726bc3a44068dfb6a9
Suwath Ch <suwatch@microsoft.com>
2021-03-31 14:18:46 -0700

    support other X509KeyStorageFlags

--- InMemoryX509Certificate.cs
+++ InMemoryX509Certificate.cs
@@ -16,26 +16,27 @@ namespace InMemoryX509Certificate
 
         readonly WeakReference m_safeCertContext;
 
-        public InMemoryX509Certificate(byte[] rawData, string password)
+        public InMemoryX509Certificate(byte[] rawData, string password, X509KeyStorageFlags keyStorageFlags = X509KeyStorageFlags.DefaultKeySet)
         {
             m_safeCertContext = new WeakReference(SafeCertContextField.GetValue(this));
-            LoadCertificateFromBlob(rawData, password);
+            LoadCertificateFromBlob(rawData, password, keyStorageFlags);
         }
 
-        public InMemoryX509Certificate(string fileName, string password)
+        public InMemoryX509Certificate(string fileName, string password, X509KeyStorageFlags keyStorageFlags = X509KeyStorageFlags.DefaultKeySet)
         {
             m_safeCertContext = new WeakReference(SafeCertContextField.GetValue(this));
-            LoadCertificateFromFile(fileName, password);
+            LoadCertificateFromFile(fileName, password, keyStorageFlags);
         }
 
-        private void LoadCertificateFromBlob(byte[] rawData, string password)
+        private void LoadCertificateFromBlob(byte[] rawData, string password, X509KeyStorageFlags keyStorageFlags)
         {
+            uint flags = MapKeyStorageFlags(keyStorageFlags);
             IntPtr intPtr = IntPtr.Zero;
             RuntimeHelpers.PrepareConstrainedRegions();
             try
             {
                 intPtr = Marshal.StringToHGlobalUni(password);
-                LoadCertFromBlob.Invoke(null, new object[] { rawData, intPtr, EphemeralKeySetFlag, false, m_safeCertContext.Target });
+                LoadCertFromBlob.Invoke(null, new object[] { rawData, intPtr, flags, false, m_safeCertContext.Target });
             }
             finally
             {
@@ -46,14 +47,15 @@ namespace InMemoryX509Certificate
             }
         }
 
-        private void LoadCertificateFromFile(string fileName, string password)
+        private void LoadCertificateFromFile(string fileName, string password, X509KeyStorageFlags keyStorageFlags)
         {
+            uint flags = MapKeyStorageFlags(keyStorageFlags);
             IntPtr intPtr = IntPtr.Zero;
             RuntimeHelpers.PrepareConstrainedRegions();
             try
             {
                 intPtr = Marshal.StringToHGlobalUni(password);
-                LoadCertFromFile.Invoke(null, new object[] { fileName, intPtr, EphemeralKeySetFlag, false, m_safeCertContext.Target });
+                LoadCertFromFile.Invoke(null, new object[] { fileName, intPtr, flags, false, m_safeCertContext.Target });
             }
             finally
             {
@@ -62,6 +64,36 @@ namespace InMemoryX509Certificate
                     Marshal.ZeroFreeGlobalAllocUnicode(intPtr);
                 }
             }
+        }
+
+        private static uint MapKeyStorageFlags(X509KeyStorageFlags keyStorageFlags)
+        {
+            if (keyStorageFlags == (keyStorageFlags & X509KeyStorageFlags.PersistKeySet))
+            {
+                throw new ArgumentException("X509KeyStorageFlags.PersistKeySet is not supported for InMemoryX509Certificate");
+            }
+
+            // always ephemeral
+            uint flags = EphemeralKeySetFlag;
+            if ((keyStorageFlags & X509KeyStorageFlags.UserKeySet) == X509KeyStorageFlags.UserKeySet)
+            {
+                flags |= 0x1000;
+            }
+            else if ((keyStorageFlags & X509KeyStorageFlags.MachineKeySet) == X509KeyStorageFlags.MachineKeySet)
+            {
+                flags |= 0x20;
+            }
+
+            if ((keyStorageFlags & X509KeyStorageFlags.Exportable) == X509KeyStorageFlags.Exportable)
+            {
+                flags |= 1;
+            }
+            if ((keyStorageFlags & X509KeyStorageFlags.UserProtected) == X509KeyStorageFlags.UserProtected)
+            {
+                flags |= 2;
+            }
+
+            return flags;
         }
     }
 }
